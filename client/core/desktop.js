8d716a724d7d034e61a34d44a44edec1ea5d87be
Daniel Esteban Nombela <dani@gatunes.com>
2021-01-12 08:27:34 +0100

    Change desktop controls direction to world axis

--- client/core/desktop.js
+++ client/core/desktop.js
@@ -109,6 +109,8 @@ class DesktopControls {
         worldUp,
       } = this.aux;
       camera.getWorldDirection(forward);
+      forward.y = 0;
+      forward.normalize();
       right.crossVectors(forward, worldUp);
       player.move(
         direction
