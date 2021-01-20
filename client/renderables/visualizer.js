ec82457b2f87a1f16133a2dbc8b869939aa5e987
Daniel Esteban Nombela <dani@gatunes.com>
2021-01-20 17:43:35 +0100

    Cooler visualizer

--- client/renderables/visualizer.js
+++ client/renderables/visualizer.js
@@ -4,24 +4,47 @@ class Visualizer {
     renderer.style.marginBottom = '1.25rem';
     renderer.width = 200;
     renderer.height = 50;
+    this.bar = {
+      y: renderer.height / Visualizer.steps,
+    };
+    this.bar.h = this.bar.y * 0.8;
+    this.bar.oy = (this.bar.y - this.bar.h) * 0.5;
     this.context = renderer.getContext('2d');
-    const bar = this.context.createLinearGradient(0, 0, 0, renderer.height);
-    bar.addColorStop(0, 'rgba(51, 153, 51, 1)');
-    bar.addColorStop(1, 'rgba(51, 153, 51, 0)');
-    this.context.fillStyle = bar;
+    this.gradients = {
+      on: this.context.createLinearGradient(0, 0, 0, renderer.height),
+      off: this.context.createLinearGradient(0, 0, 0, renderer.height),
+    };
+    this.gradients.on.addColorStop(0, 'rgba(51, 153, 51, 1)');
+    this.gradients.on.addColorStop(1, 'rgba(51, 153, 51, 0)');
+    this.gradients.off.addColorStop(0, 'rgba(0, 0, 0, 0.125)');
+    this.gradients.off.addColorStop(1, 'rgba(0, 0, 0, 0.25)');
     this.renderer = renderer;
   }
 
   update(bins) {
-    const { context: ctx, renderer } = this;
+    const { bar, context: ctx, gradients, renderer } = this;
+    const { on, off } = gradients;
     const { width, height } = renderer;
-    const bar = width / bins.length;
+    const { steps } = Visualizer;
+    bar.x = width / bins.length;
+    bar.w = bar.x * 0.9;
+    bar.ox = (bar.x - bar.w) * 0.5;
     ctx.clearRect(0, 0, width, height);
     bins.forEach((value, i) => {
-      const h = height * value;
-      ctx.fillRect(bar * i, height - h, bar, h);
+      const step = steps * value;
+      for (let j = 0; j < steps; j += 1) {
+        ctx.fillStyle = j < step ? on : off;
+        ctx.fillRect(
+          bar.x * i + bar.ox,
+          height - (bar.y * (j + 1) + bar.oy),
+          bar.w,
+          bar.h
+        );
+      }
     });
   }
 }
+
+Visualizer.steps = 10;
 
 export default Visualizer;
