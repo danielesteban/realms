a2a01c5eed5fa5ca1ff07d228b58ae9e097852c8
Daniel Esteban Nombela <dani@gatunes.com>
2021-01-27 11:59:54 +0100

    Upgrade three to rev 125

--- client/core/three.js
+++ client/core/three.js
@@ -1,3 +1,3 @@
-export * from 'https://cdn.jsdelivr.net/npm/three@0.124.0/build/three.module.js';
-export { GLTFExporter } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/exporters/GLTFExporter.js';
-export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/utils/BufferGeometryUtils.js';
+export * from 'https://cdn.jsdelivr.net/npm/three@0.125.1/build/three.module.js';
+export { GLTFExporter } from 'https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/exporters/GLTFExporter.js';
+export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/utils/BufferGeometryUtils.js';
